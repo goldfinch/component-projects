b2f40240c05a7d3e0fb59b77f692611753d95a2b
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-05 15:49:08 +1300

    WIP

--- src/Models/Nest/ProjectCategory.php
+++ src/Models/Nest/ProjectCategory.php
@@ -42,7 +42,7 @@ class ProjectCategory extends NestedObject
 
     public function fielder(Fielder $fielder): void
     {
-        $fielder->require(['Title']);
+        $fielder->required(['Title']);
 
         $fielder->fields([
             'Root.Main' => [
