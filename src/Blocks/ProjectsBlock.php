914b0730adce1f69888bfbd11d502ccef7bba08c
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-07 08:27:52 +1300

    WIP

--- src/Blocks/ProjectsBlock.php
+++ src/Blocks/ProjectsBlock.php
@@ -2,17 +2,12 @@
 
 namespace Goldfinch\Component\Projects\Blocks;
 
-use Goldfinch\Fielder\Fielder;
-use Goldfinch\Mill\Traits\Millable;
-use Goldfinch\Fielder\Traits\FielderTrait;
-use DNADesign\Elemental\Models\BaseElement;
+use Goldfinch\Blocks\Models\BlockElement;
 use Goldfinch\Component\Projects\Models\Nest\ProjectItem;
 use Goldfinch\Component\Projects\Models\Nest\ProjectCategory;
 
-class ProjectsBlock extends BaseElement
+class ProjectsBlock extends BlockElement
 {
-    use FielderTrait, Millable;
-
     private static $table_name = 'ProjectsBlock';
     private static $singular_name = 'Projects';
     private static $plural_name = 'Projects';
@@ -23,11 +18,6 @@ class ProjectsBlock extends BaseElement
     private static $description = '';
     private static $icon = 'font-icon-block-accordion';
 
-    public function fielder(Fielder $fielder): void
-    {
-        // ..
-    }
-
     public function Items()
     {
         return ProjectItem::get();
@@ -36,17 +26,5 @@ class ProjectsBlock extends BaseElement
     public function Categories()
     {
         return ProjectCategory::get();
-    }
-
-    public function getSummary()
-    {
-        return $this->getDescription();
-    }
-
-    public function getType()
-    {
-        $default = $this->i18n_singular_name() ?: 'Block';
-
-        return _t(__CLASS__ . '.BlockType', $default);
     }
 }
