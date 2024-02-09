61bf23cd09320c5dcaeef6dd39d8b09f529e06d0
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 11:25:57 +1300

    WIP

--- src/Blocks/ProjectsBlock.php
+++ src/Blocks/ProjectsBlock.php
@@ -2,11 +2,11 @@
 
 namespace Goldfinch\Component\Projects\Blocks;
 
-use Goldfinch\Blocks\Models\BlockElement;
+use DNADesign\Elemental\Models\BaseElement;
 use Goldfinch\Component\Projects\Models\Nest\ProjectItem;
 use Goldfinch\Component\Projects\Models\Nest\ProjectCategory;
 
-class ProjectsBlock extends BlockElement
+class ProjectsBlock extends BaseElement
 {
     private static $table_name = 'ProjectsBlock';
     private static $singular_name = 'Projects';
@@ -15,7 +15,7 @@ class ProjectsBlock extends BlockElement
     private static $db = [];
 
     private static $inline_editable = false;
-    private static $description = '';
+    private static $description = 'Projects block handler';
     private static $icon = 'font-icon-block-accordion';
 
     public function Items()
